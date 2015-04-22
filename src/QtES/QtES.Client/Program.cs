960b4401b33346674500d9f6a1abd798aad9a816
Kenneth Devik <kenneth@devik.net>
2015-04-22 15:39:49 +0200

    Påbegynt service for å lese ut meldinger fra MQ til ES

---
+++ src/QtES/QtES.Client/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace QtES.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var message = string.Empty;
+            System.Console.WriteLine("Opprett nye Rabbit-meldinger ved å skrive noe og trykk <enter>. CTRL+C for å avbryte");
+            while (!string.IsNullOrEmpty(message = System.Console.ReadLine()))
+            {
+                try
+                {
+                    var factory = new ConnectionFactory() {HostName = "localhost"};
+                    using (var connection = factory.CreateConnection())
+                    using (var channel = connection.CreateModel())
+                    {
+                        channel.ExchangeDeclare("logs", "fanout");
+                        var body = Encoding.UTF8.GetBytes(message);
+                        channel.BasicPublish("logs", "", null, body);
+                        System.Console.WriteLine("OK. Klar for ny melding");
+                    }
+                }
+                catch (Exception e)
+                {
+                    System.Console.WriteLine("Feil. " + e.Message);
+                }
+            }
+        }
+    }
+}
---
+++ src/QtES/QtES.Service/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.ServiceProcess;
+
+namespace QtES.Service
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            var service = new Service1();
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[] 
+            { 
+                
+            };
+            service.Start(args);
+# if Debug 
+            Serv
+# endif
+# if Release
+            ServiceBase.Run(ServicesToRun);
+# endif
+        }
+    }
+}
